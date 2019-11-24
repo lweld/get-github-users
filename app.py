f92f56498242d00c09306e3795b8d07ecb295724
Liam Weld <liamjweld@gmail.com>
2019-11-24 15:35:40 -0800

    allow entering repos

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ clearbit.key = os.getenv("CLEARBIT_KEY")
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 
-def get_contributors(repos_list):
+def get_org_contributors(repos_list):
 	contributors_list = []
 	for repo in repos_list:
 		repo_contributors = []
@@ -25,16 +25,33 @@ def get_contributors(repos_list):
 		contributors_list.append(repo_contributors)
 	return contributors_list
 
-def get_github_handles(contributors_list):
+def get_repo_contributors(repo):
+	contributors_list = []
+	contributors_list.append(repo["contributors_url"])
+	contributors_list.append(repo["forks_url"])
+	contributors_list.append(repo["stargazers_url"])
+	return contributors_list
+
+def get_org_github_handles(contributors_list):
 	github_handles = []
 	for repo in contributors_list:
 		contributors = requests.get(url = "{}?client_id={}&client_secret={}".format(repo[0], client_id, client_secret))
 		forks = requests.get(url = "{}?client_id={}&client_secret={}".format(repo[1], client_id, client_secret))
 		stars = requests.get(url = "{}?client_id={}&client_secret={}".format(repo[2], client_id, client_secret))
-		for user in contributors.json(): 
-			github_handles.append(user["login"])
+		for user in contributors.json(): github_handles.append(user["login"])
 		for user in forks.json(): github_handles.append(user["owner"]["login"])
 		for user in stars.json(): github_handles.append(user["login"])
+	github_handles = list(set(github_handles))
+	return github_handles
+
+def get_repo_github_handles(contributors_list):
+	github_handles = []
+	contributors = requests.get(url = "{}?client_id={}&client_secret={}".format(contributors_list[0], client_id, client_secret))
+	forks = requests.get(url = "{}?client_id={}&client_secret={}".format(contributors_list[1], client_id, client_secret))
+	stars = requests.get(url = "{}?client_id={}&client_secret={}".format(contributors_list[2], client_id, client_secret))
+	for user in contributors.json(): github_handles.append(user["login"])
+	for user in forks.json(): github_handles.append(user["owner"]["login"])
+	for user in stars.json(): github_handles.append(user["login"])
 	github_handles = list(set(github_handles))
 	return github_handles
 
@@ -77,18 +94,44 @@ def enrich_emails(emails):
 @app.route('/', methods=['GET', 'POST'])
 def form_example():
 	if request.method == 'POST':
-		org = request.form.get('github_org')
-		github_url = "https://api.github.com/orgs/{}/repos?client_id={}&client_secret={}".format(org, client_id, client_secret)
-		r = requests.get(url = github_url).json()
-		if "message" in r:
-			return '''<form method="POST">
-              		  GitHub Org: <input type="text" name="github_org" placeholder="stripe-ctf">
-              		  <input type="submit" value="Submit"><br>
-          		  </form>
-          		  <p>It'll take a few mins for the csv to be generated.<p>
-				  <h2>"{}" is not a valid organization on GitHub.<h2>'''.format(org)
-		contributors_list = get_contributors(r)
-		github_handles = get_github_handles(contributors_list)
+		if request.form.get('github_org') != "":
+			try:
+				org = request.form.get('github_org').split('/')[-1]
+			except:
+				org = request.form.get('github_org')
+			github_url = "https://api.github.com/orgs/{}/repos?client_id={}&client_secret={}".format(org, client_id, client_secret)
+			r = requests.get(url = github_url).json()
+			if "message" in r:
+				return '''<form method="POST" style="font-family: arial">
+	              		  GitHub Organization: <input type="text" name="github_org" placeholder="https://github.com/stripe-ctf" style="width: 16%">
+              			  <input type="submit" value="Submit" style="width: 60px font-family: arial" ><br><br>
+              			  GitHub Repository: <input type="text" name="github_repo" placeholder="https://github.com/stripe-ctf/octopus" style="width: 17.1%">
+              			  <input type="submit" value="Submit" style="width: 60px font-family: arial"><br>
+	          		  </form>
+	          		  <p style="font-family: arial">It'll take a few mins for the csv to be generated.<p>
+					  <h2 style="font-family: arial">"{}" is not a valid organization on GitHub.<h2>'''.format(org)
+			contributors_list = get_org_contributors(r)
+			github_handles = get_org_github_handles(contributors_list)
+		else:
+			try:
+				org = request.form.get('github_repo').split('/')[-2]
+				repo = request.form.get('github_repo').split('/')[-1]
+			except:
+				org = request.form.get('github_repo')
+				repo = request.form.get('github_repo')
+			github_url = "https://api.github.com/repos/{}/{}?client_id={}&client_secret={}".format(org, repo, client_id, client_secret)
+			r = requests.get(url = github_url).json()
+			if "message" in r:
+				return '''<form method="POST" style="font-family: arial">
+	              		  GitHub Organization: <input type="text" name="github_org" placeholder="https://github.com/stripe-ctf" style="width: 16%">
+              			  <input type="submit" value="Submit" style="width: 60px font-family: arial" ><br><br>
+              			  GitHub Repository: <input type="text" name="github_repo" placeholder="https://github.com/stripe-ctf/octopus" style="width: 17.1%">
+              			  <input type="submit" value="Submit" style="width: 60px font-family: arial"><br>
+	          		  </form>
+	          		  <p style="font-family: arial">It'll take a few mins for the csv to be generated.<p>
+					  <h2 style="font-family: arial">"{}/{}" is not a valid repository on GitHub.<h2>'''.format(org, repo)
+			contributors_list = get_repo_contributors(r)
+			github_handles = get_repo_github_handles(contributors_list)
 		emails = get_emails(github_handles)
 		users = enrich_emails(emails)
 		df = pd.DataFrame(users)
@@ -97,11 +140,13 @@ def form_example():
 		resp.headers["Content-Type"] = "text/csv"
 		return resp
 		
-	return '''<form method="POST">
-              GitHub Org: <input type="text" name="github_org" placeholder="stripe-ctf">
-              <input type="submit" value="Submit"><br>
+	return '''<form method="POST" style="font-family: arial">
+              GitHub Organization: <input type="text" name="github_org" placeholder="https://github.com/stripe-ctf" style="width: 16%">
+              <input type="submit" value="Submit" style="width: 60px font-family: arial" ><br><br>
+              GitHub Repository: <input type="text" name="github_repo" placeholder="https://github.com/stripe-ctf/octopus" style="width: 17.1%">
+              <input type="submit" value="Submit" style="width: 60px font-family: arial"><br>
           </form>
-          <p>It'll take a few mins for the csv to be generated.<p>'''
+          <p style="font-family: arial">It'll take a few mins for the csv to be generated.<p>'''
 
 if __name__ == "__main__":
     app.run(debug=True)
